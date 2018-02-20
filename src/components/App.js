015c796121984e9d989b8794273356d40df43203
gerardramosayasdi <gerard.ramosm89@gmail.com>
2018-02-19 21:52:26 -0800

    adding addgift test

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gifts: [
+
+      ]
+    }
+  }
+  
+  addGift = () => {
+    const { gifts } = this.state;
+    const newGifts = gifts.slice();
+    newGifts.push({ id: 1 });
+    this.setState({
+      gifts: newGifts
+    });
+  }
+  
   render() {
     return(
       <div>
         <h2>Gift Giver</h2>
+        <Button className="btn-add" onClick={this.addGift}>Add Gift</Button>
       </div>
     );
   }
